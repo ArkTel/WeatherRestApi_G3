2450b02a64bc3182b60e7149098fbda351fdb41a
ArkTel <arkadio071@wp.pl>
2021-08-18 23:03:31 +0200

    feature(wra): adding entity classes

--- src/main/java/backend/model/Location.java
+++ src/main/java/backend/model/Location.java
@@ -1,4 +1,17 @@
 package backend.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "location")
 public class Location {
+    private String name;
+    private String latitude;
+    private String longitude;
+    private String region;
+    private String country;
 }
--- src/main/java/backend/model/WeatherDate.java
+++ src/main/java/backend/model/WeatherDate.java
@@ -1,4 +1,22 @@
 package backend.model;
 
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "weather_date")
 public class WeatherDate {
+    private double temperature;
+    private double pressure;
+    private double humidity;
+
+    @Column(name = "wind_speed")
+    private double windSpeed;
+
+    @Column(name = "wind_direction")
+    private String windDirection;
 }
