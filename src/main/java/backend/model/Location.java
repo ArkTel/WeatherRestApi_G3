ce06bd3d9ffc06c1bad5bbfdb976efd86e459dd5
ArkTel <arkadio071@wp.pl>
2021-08-19 18:32:59 +0200

    change(WRA): adding field id

--- src/main/java/backend/model/Location.java
+++ src/main/java/backend/model/Location.java
@@ -3,12 +3,18 @@ package backend.model;
 import lombok.Data;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Data
 @Entity
 @Table(name = "location")
 public class Location {
+    @Id
+    @GeneratedValue
+    private int id;
+
     private String name;
     private String latitude;
     private String longitude;
