0780fef3109761e89dc47cb956fd40ca71ef7133
Kucm4333 <marcinkuc99@gmail.com>
2021-08-24 19:52:07 +0200

    feature(wra):add connect class with weatherstack api

--- src/main/java/backend/connect/WetherstackConnection.java
+++ src/main/java/backend/connect/WetherstackConnection.java
@@ -1,2 +1,34 @@
-package backend.connect;public class WetherstackConnection {
+package backend.connect;
+
+import okhttp3.Call;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class WetherstackConnection {
+
+    public static String connect(String cityName) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        String BASE_URL = String.format("http://api.weatherstack.com/forecast" +
+                "?access_key=265f4183f04e4ff29b09ad16dc205aaa" +
+                "&query=%s" +
+                "&forecast_days=1" +
+                "&hourly=1", cityName);
+
+        Request request = new Request.Builder()
+                .url(BASE_URL)
+                .build();
+
+        OkHttpClient client = new OkHttpClient();
+
+        Call call = client.newCall(request);
+        Response response;
+        response = call.execute();
+
+        return response.body().string();
+    }
 }
